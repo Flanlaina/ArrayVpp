b673519ddb6eb6102e74a231ce994fb713857228
Quang Lam <hchuc2463@gmail.com>
2026-09-04 21:50:04 +0700

    add original slope sort

--- src/main/java/io/github/arrayv/sorts/exchange/SlopeSort.java
+++ src/main/java/io/github/arrayv/sorts/exchange/SlopeSort.java
@@ -1,50 +1,47 @@
+/**
+ *
+ */
 package io.github.arrayv.sorts.exchange;
 
 import io.github.arrayv.main.ArrayVisualizer;
 import io.github.arrayv.sorts.templates.Sort;
 
 /**
- * @author Flanlaina
+ * @author McDude_73
  * @author EilrahcF
  *
  */
-public class SlopeSort extends Sort {
+public final class SlopeSort extends Sort {
 
-    public SlopeSort(ArrayVisualizer arrayVisualizer) {
-        super(arrayVisualizer);
-        setSortListName("Slope");
-        setRunAllSortsName("Slope Sort");
-        setRunSortName("Slopesort");
-        setCategory("Exchange Sorts");
-        setAuthors("EilrahcF, Flanlaina");
-        setBucketSort(false);
-        setRadixSort(false);
-        setUnreasonablySlow(false);
-        setUnreasonableLimit(0);
-        setBogoSort(false);
-    }
-    
-    public void sort(int[] array, int a, int b) {
-        for (int i = a + 1; i < b; i++)
-            for (int j = i; j > a; j--)
-                if (Reads.compareIndices(array, j, j - 1, 0.04, true) < 0)
-                    Writes.swap(array, j - 1, j, 0.02, true, false);
-    }
+	/**
+	 * @param arrayVisualizer
+	 */
+	public SlopeSort(ArrayVisualizer arrayVisualizer) {
+		super(arrayVisualizer);
+		setSortListName("Slope");
+		setRunAllSortsName("Slope Sort");
+		setRunSortName("Slopesort");
+		setCategory("Exchange Sorts");
+		setBucketSort(false);
+		setRadixSort(false);
+		setUnreasonablySlow(true);
+		setUnreasonableLimit(16384);
+		setBogoSort(false);
+		this.setAuthors("McDude_73, Flanlaina, EilrahcF");
 
-    @Override
-    public void runSort(int[] array, int length, int bucketCount) {
-        /*
-        for (int i = 1, j = 1; i < length; i++, j++) {
-            for (int k = i - 1; k >= 0; k--, i--) {
-                if (this.Reads.compareIndices(array, i, k, 0.04D, true) < 0) {
-                    this.Writes.swap(array, i, k, 0.02D, true, false);
-                }
-            }
-            i = j;
-        }
-        */
-        sort(array, 0, length);
+	}
 
-    }
+	@Override
+	public void runSort(int[] array, int length, int bucketCount) {
+		for (int i = 1, j = 1; i < length; i++, j++) {
+			for (int k = i - 1; k >= 0; k--, i--) {
+				if (this.Reads.compareIndices(array, i, k, 0.04D, true) < 0) {
+					this.Writes.swap(array, i, k, 0.02D, true, false);
+				}
+			}
+			i = j;
+		}
+
+	}
 
 }
---
+++ src/main/java/io/github/arrayv/sorts/exchange/SlopeSortFlanlaina.java
@@ -0,0 +1,50 @@
+package io.github.arrayv.sorts.exchange;
+
+import io.github.arrayv.main.ArrayVisualizer;
+import io.github.arrayv.sorts.templates.Sort;
+
+/**
+ * @author Flanlaina
+ * @author EilrahcF
+ *
+ */
+public class SlopeSortFlanlaina extends Sort {
+
+    public SlopeSortFlanlaina(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+        setSortListName("Slope (Flanlaina's update)");
+        setRunAllSortsName("Slope Sort");
+        setRunSortName("Slopesort");
+        setCategory("Exchange Sorts");
+        setAuthors("EilrahcF, Flanlaina");
+        setBucketSort(false);
+        setRadixSort(false);
+        setUnreasonablySlow(false);
+        setUnreasonableLimit(0);
+        setBogoSort(false);
+    }
+    
+    public void sort(int[] array, int a, int b) {
+        for (int i = a + 1; i < b; i++)
+            for (int j = i; j > a; j--)
+                if (Reads.compareIndices(array, j, j - 1, 0.04, true) < 0)
+                    Writes.swap(array, j - 1, j, 0.02, true, false);
+    }
+
+    @Override
+    public void runSort(int[] array, int length, int bucketCount) {
+        /*
+        for (int i = 1, j = 1; i < length; i++, j++) {
+            for (int k = i - 1; k >= 0; k--, i--) {
+                if (this.Reads.compareIndices(array, i, k, 0.04D, true) < 0) {
+                    this.Writes.swap(array, i, k, 0.02D, true, false);
+                }
+            }
+            i = j;
+        }
+        */
+        sort(array, 0, length);
+
+    }
+
+}
